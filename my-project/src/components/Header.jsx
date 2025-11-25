428371f2af6e21d692c4d43b2a25f454698d623e
Debfevlo <deborahfevlo@gmail.com>
2025-11-25 22:30:33 +0000

    make changes in header component

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,10 +1,9 @@
-
 import SearchBar from "./SearchBar"
 
 
 const Header = () => {
   return (
-    <div className=" bg-[url(images/pattern-bg-mobile.png)] md:bg-[url(images/pattern-bg-desktop.png)] bg-center h-72 md:h-60  text-white  text-center bg-cover p-8 relative">
+    <div className=" bg-[url('/images/pattern-bg-mobile.png')] md:bg-[url('/images/pattern-bg-desktop.png')] bg-center h-72 md:h-60  text-white  text-center bg-cover p-8 relative">
         <p className=" text-3xl md:text-4xl font-semibold"> IP Address Tracker</p>
         <SearchBar/>
         
